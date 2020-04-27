76b95499e8dcaa6fb2ac4f16c0208efcccc4602a
Arun <63234884+iamarun0246@users.noreply.github.com>
2020-04-27 13:41:12 +0530

    started

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// --- Select menu ---
+var nua = navigator.userAgent;
+var is_android = ((nua.indexOf('Mozilla/5.0') > -1 && nua.indexOf('Android ') > -1 && nua.indexOf('AppleWebKit') > -1) && !(nua.indexOf('Chrome') > -1));
+if(is_android) {
+		$('select.form-control').removeClass('form-control').css('width', '100%');
+
+}
+// -- !Select menu --
